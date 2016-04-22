2ae2273d035c71df8c5a1d1b9b702f347ae30245
jeremykendall <jeremy@jeremykendall.net>
2016-04-22 13:43:25 -0500

    Add extractions

--- src/Repository/ExtractData.php
+++ src/Repository/ExtractData.php
@@ -1,24 +1,68 @@
 <?php
 
+/**
+ * OSMI Survey Graph project.
+ *
+ * @link https://github.com/OSMIHelp/osmi-survey-graph
+ */
 namespace OSMI\Survey\Graph\Repository;
 
+use GraphAware\Neo4j\Client\Exception\Neo4jException;
+
+/**
+ * Creates a "graphy" model from the survey results, allowing for easier survey data analysis.
+ *
+ * This is probably a good start, and could definitely be fleshed out a little
+ * more. If it's difficult to write a query for something because one has to
+ * dig into the questions and answers, it might be good to add a new extraction
+ * here.
+ */
 class ExtractData extends Neo4j
 {
+    /**
+     * Performs the data extraction.
+     */
     public function extractData()
     {
         $this->client->run($this->getCreateSurvey());
         $this->client->run($this->getRemoveDuplicatedQuestions());
+        $this->updateQuestionFieldIdSchema();
         $this->client->run($this->getCreateListOfQuestions());
         $this->client->run($this->getCreateSurveyLinkedList());
         $this->client->run($this->getCreateCountries());
         $this->client->run($this->getCreateStates());
+        $this->client->run($this->getCreateWorksInCountry());
+        $this->client->run($this->getCreateLivesInCountry());
+        $this->client->run($this->getCreateWorksInState());
+        $this->client->run($this->getCreateLivesInState());
+        $this->client->run($this->getCreateWorksAs());
+        $this->client->run($this->getCreateStates());
+        $this->client->run($this->getCreateWorksAs());
+        $this->client->run($this->getCreateCurrentDiagnosis());
+        $this->client->run($this->getCreateSelfDiagnosis());
+        $this->client->run($this->getCreateProfessionalDiagnosis());
     }
 
+    /**
+     * Cypher query to create a Survey node.
+     *
+     * @return string CQL
+     */
     public function getCreateSurvey()
     {
         return 'MERGE (s:Survey { id: 1 }) SET s.year = 2016';
     }
 
+    /**
+     * Cypher query to remove duplicate question nodes.
+     *
+     * The survey data includes some few duplicated questions due to the way
+     * some questions and choices are related (Ex. "If yes, what condition(s)
+     * have you been diagnosed with?"). This query consolidates those duplicate
+     * nodes and moves all the ANSWER relationships to that single node.
+     *
+     * @return string CQL
+     */
     public function getRemoveDuplicatedQuestions()
     {
         return <<<CQL
@@ -44,6 +88,11 @@ CREATE UNIQUE (keep)-[:HAS_ANSWER]->(a);
 CQL;
     }
 
+    /**
+     * Creates list of questions in asked order.
+     *
+     * @return string CQL
+     */
     public function getCreateListOfQuestions()
     {
         return <<<CQL
@@ -57,6 +106,12 @@ MERGE (q1)-[:QUESTION]->(q2)
 CQL;
     }
 
+    /**
+     * Finishes creating linked list by attaching the Question list to the
+     * Survey node.
+     *
+     * @return string CQL
+     */
     public function getCreateSurveyLinkedList()
     {
         return <<<CQL
@@ -69,6 +124,11 @@ CREATE UNIQUE (last)-[:QUESTION]->(s)-[:QUESTION]->(first)
 CQL;
     }
 
+    /**
+     * Creates Country nodes present in survey responses.
+     *
+     * @return string CQL
+     */
     public function getCreateCountries()
     {
         return <<<CQL
@@ -83,6 +143,11 @@ MERGE (p)-[:CHILD]->(c);
 CQL;
     }
 
+    /**
+     * Creates State nodes present in survey responses.
+     *
+     * @return string CQL
+     */
     public function getCreateStates()
     {
         return <<<CQL
@@ -95,5 +160,188 @@ WITH s
 MATCH (c:Country { name: 'United States of America' })
 MERGE (c)-[:CHILD]->(s);
 CQL;
+    }
+
+    /**
+     * Create LIVES_IN Country relationships.
+     *
+     * @return string CQL
+     */
+    public function getCreateLivesInCountry()
+    {
+        return <<<CQL
+MATCH (q:Question { id: "dropdown_18069141" })-[:HAS_ANSWER]->(a)
+WITH DISTINCT a
+MATCH (a)<-[:ANSWERED]-(p)
+// If answer is United States of America, then answer _must_ include a state,
+// so ignore the US answers here.
+WHERE NOT a.answer = "United States of America"
+WITH a, COLLECT(p) AS residents
+MATCH (c:Country { name: a.answer })
+WITH c, residents
+UNWIND residents AS resident
+CREATE UNIQUE (resident)-[:LIVES_IN]->(c);
+CQL;
+    }
+
+    /**
+     * Create WORKS_IN Country relationships.
+     *
+     * @return string CQL
+     */
+    public function getCreateWorksInCountry()
+    {
+        return <<<CQL
+PROFILE
+MATCH (q:Question { id: "dropdown_18069205" })-[:HAS_ANSWER]->(a)
+WITH DISTINCT a
+MATCH (a)<-[:ANSWERED]-(p)
+// If answer is United States of America, then answer _must_ include a state,
+// so ignore the US answers here.
+WHERE NOT a.answer = "United States of America"
+WITH a, COLLECT(p) AS residents
+MATCH (c:Country { name: a.answer })
+WITH c, residents
+UNWIND residents AS resident
+CREATE UNIQUE (resident)-[:WORKS_IN]->(c);
+CQL;
+    }
+
+    /**
+     * Creates LIVES_IN State relationships.
+     *
+     * @return string CQL
+     */
+    public function getCreateLivesInState()
+    {
+        return <<<CQL
+MATCH (q:Question { id: "dropdown_18069210" })-[:HAS_ANSWER]->(a)
+WITH a
+MATCH (a)<-[:ANSWERED]-(p)
+WITH a, COLLECT(p) AS residents
+MATCH (s:State { name: a.answer })
+WITH s, residents
+UNWIND residents AS resident
+CREATE UNIQUE (resident)-[:LIVES_IN]->(s)
+CQL;
+    }
+
+    /**
+     * Creates WORKS_IN State relationships.
+     *
+     * @return string CQL
+     */
+    public function getCreateWorksInState()
+    {
+        return <<<CQL
+MATCH (q:Question { id: "dropdown_18069285" })-[:HAS_ANSWER]->(a)
+WITH a
+MATCH (a)<-[:ANSWERED]-(p)
+WITH a, COLLECT(p) AS residents
+MATCH (s:State { name: a.answer })
+WITH s, residents
+UNWIND residents AS resident
+CREATE UNIQUE (resident)-[:WORKS_IN]->(s)
+CQL;
+    }
+
+    /**
+     * Creates WORKS_AS relationships.
+     *
+     * @return string CQL
+     */
+    public function getCreateWorksAs()
+    {
+        return <<<CQL
+MATCH (q:Question { id: "list_18069282_choice_23064638" })-[:HAS_ANSWER]->(a)<-[:ANSWERED]-(p)
+WITH a, COLLECT(p) AS respondents
+MERGE (profession:Profession { name: a.answer })
+WITH profession, respondents
+UNWIND respondents AS respondent
+MERGE (respondent)-[:WORKS_AS]->(profession)
+CQL;
+    }
+
+    /**
+     * Creates the CURRENT_DIAGNOSIS relationships.
+     *
+     * @return string CQL
+     */
+    public function getCreateCurrentDiagnosis()
+    {
+        return <<<CQL
+MATCH (q:Question { question: "If yes, what condition(s) have you been diagnosed with?" })-[:HAS_ANSWER]->(a)
+WITH a
+MATCH (a)<-[:ANSWERED]-(p)
+WITH a, COLLECT(p) AS respondents
+MERGE (d:Disorder { name: a.answer })
+WITH d, respondents
+UNWIND respondents AS respondent
+CREATE UNIQUE (respondent)-[:CURRENT_DIAGNOSIS]->(d);
+CQL;
+    }
+
+    /**
+     * Creates the SELF_DIAGNOSIS relationships.
+     *
+     * @return string CQL
+     */
+    public function getCreateSelfDiagnosis()
+    {
+        return <<<CQL
+MATCH (q:Question { question: "If maybe, what condition(s) do you believe you have?" })-[:HAS_ANSWER]->(a)
+WITH a
+MATCH (a)<-[:ANSWERED]-(p)
+WITH a, COLLECT(p) AS respondents
+MERGE (d:Disorder { name: a.answer })
+WITH d, respondents
+UNWIND respondents AS respondent
+CREATE UNIQUE (respondent)-[:SELF_DIAGNOSIS]->(d);
+CQL;
+    }
+
+    /**
+     * Creates the PROFESSIONAL_DIAGNOSIS relationships.
+     *
+     * @return string CQL
+     */
+    public function getCreateProfessionalDiagnosis()
+    {
+        return <<<CQL
+MATCH (q:Question { question: "If so, what condition(s) were you diagnosed with?" })-[:HAS_ANSWER]->(a)
+WITH a
+MATCH (a)<-[:ANSWERED]-(p)
+WITH a, COLLECT(p) AS respondents
+MERGE (d:Disorder { name: a.answer })
+WITH d, respondents
+UNWIND respondents AS respondent
+CREATE UNIQUE (respondent)-[:PROFESSIONAL_DIAGNOSIS]->(d);
+CQL;
+    }
+
+    /**
+     * Updates schema on :Question(field_id).
+     *
+     * Drops index, adds constraint.
+     */
+    public function updateQuestionFieldIdSchema()
+    {
+        try {
+            $this->client->run(sprintf('DROP INDEX ON :%s(%s)', 'Question', 'field_id'));
+        } catch (Neo4jException $e) {
+            if (strpos($e->getMessage(), 'Index belongs to constraint') === false) {
+                throw $e;
+            }
+
+            // If the property already belongs to a constraint, then the index
+            // has been already been dropped and should not be dropped again.
+            // As such one may ignore this exception.
+        }
+
+        $this->client->run(sprintf(
+            'CREATE CONSTRAINT ON (n:%s) ASSERT n.%s IS UNIQUE',
+            'Question',
+            'field_id'
+        ));
     }
 }
