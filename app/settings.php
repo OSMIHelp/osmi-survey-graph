9f5b87b9755ebb3afdaa19e81c6d756e428fa345
jeremykendall <jeremy@jeremykendall.net>
2016-07-21 14:50:22 -0500

    Remove boilerplate settings

--- app/settings.php
+++ app/settings.php
@@ -12,26 +12,8 @@ return [
                 'path' => APPLICATION_PATH . '/log/app.log',
             ],
         ],
-        'pdo' => [
-        ],
-        'doctrine' => [
-            'driver' => 'pdo_sqlite',
-            'dbname' => 'food_app_api',
-            'path' => sprintf(
-                APPLICATION_PATH . '/db/food-app-api.sq3'
-            ),
-            'port' => '',
-            'host' => '',
-            'username' => '',
-            'encoding' => 'utf8',
-            'password' => '',
-            'options' => [
-                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::ATTR_TIMEOUT => 5,
-                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-            ],
-            'proxy_dir' => APPLICATION_PATH . '/cache/Doctrine',
+        'neo4j' => [
+            'graphUrl' => getenv('GRAPH_URL'),
         ],
     ],
 ];
