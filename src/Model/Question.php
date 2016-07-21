8ebf3e1c1cf48a36e9b62a64a48e4ae77544c5bf
jeremykendall <jeremy@jeremykendall.net>
2016-07-21 14:45:32 -0500

    Add Question model

---
+++ src/Model/Question.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace OSMI\Survey\Graph\Model;
+
+use Hateoas\Configuration\Annotation as Hateoas;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * @Serializer\XmlRoot("question")
+ *
+ * @Hateoas\Relation("self", href = @Hateoas\Route(
+ *          "questions_get_one",
+ *          parameters = {
+ *              "id" = "expr(object.getId())"
+ *          }
+ *      )
+ *  )
+ */
+class Question extends AbstractModel
+{
+    /**
+     * @Serializer\XmlAttribute
+     */
+    protected $id;
+    protected $fieldId;
+    protected $question;
+    protected $order;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFieldId()
+    {
+        return $this->fieldId;
+    }
+
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
