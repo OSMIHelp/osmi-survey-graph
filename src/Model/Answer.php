99956567c539a02a485c053b62382b0016a093ce
jeremykendall <jeremy@jeremykendall.net>
2016-07-23 14:04:11 -0500

    Add rels, response counts

--- src/Model/Answer.php
+++ src/Model/Answer.php
@@ -2,10 +2,39 @@
 
 namespace OSMI\Survey\Graph\Model;
 
+use Hateoas\Configuration\Annotation as Hateoas;
 use JMS\Serializer\Annotation as Serializer;
 
 /**
  * @Serializer\XmlRoot("answer")
+ *
+ * @Hateoas\Relation("self", href = @Hateoas\Route(
+ *          "answers_get_one",
+ *          parameters = {
+ *              "hash" = "expr(object.getHash())"
+ *          }
+ *      )
+ *  )
+ *
+ * @Hateoas\Relation("respondents", href = @Hateoas\Route(
+ *          "answer_respondents",
+ *          parameters = {
+ *              "hash" = "expr(object.getHash())"
+ *          }
+ *      )
+ *  )
+ *
+ * @Hateoas\Relation("question", href = @Hateoas\Route(
+ *          "questions_get_one",
+ *          parameters = {
+ *              "id" = "expr(object.getQuestion().getId())"
+ *          }
+ *      ),
+ *      embedded = "expr(object.getQuestion())",
+ *      exclusion = @Hateoas\Exclusion(
+ *          excludeIf = "expr(object.getQuestion() === null)"
+ *      )
+ * )
  */
 class Answer extends AbstractModel
 {
@@ -14,6 +43,18 @@ class Answer extends AbstractModel
      */
     protected $hash;
     protected $answer;
+    protected $responses = 0;
+
+    /**
+     * @Serializer\Exclude
+     */
+    protected $question;
+
+    public function __construct(array $data, Question $question = null)
+    {
+        parent::__construct($data);
+        $this->question = $question;
+    }
 
     public function getHash()
     {
@@ -23,5 +64,15 @@ class Answer extends AbstractModel
     public function getAnswer()
     {
         return $this->answer;
+    }
+
+    public function getResponses()
+    {
+        return $this->responses;
+    }
+
+    public function getQuestion()
+    {
+        return $this->question;
     }
 }
